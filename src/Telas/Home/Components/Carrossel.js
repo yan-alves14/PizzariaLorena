c5f6075dd1f0d998d3822ca291de4838107c0f07
Victor Henri S. Vargas <103096096+imyudz@users.noreply.github.com>
2023-06-06 21:32:08 -0300

    Finalizando Reestruturação com Carrossel

---
+++ src/Telas/Home/Components/Carrossel.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { FlatList, Dimensions, View } from "react-native";
+
+const { width } = Dimensions.get("window");
+
+export default function Carrossel({ data }) {
+  return (
+    <FlatList
+      pagingEnabled
+      data={data}
+      snapToAlignment="start"
+      scrollEventThrottle={16}
+      decelerationRate="fast"
+      showsHorizontalScrollIndicator={false}
+      keyExtractor={(item) => String(item)}
+      horizontal
+      renderItem={({ item }) => (
+        <View
+          style={{
+            backgroundColor: item,
+            height: width / 2.6,
+            width: width,
+          }}
+        />
+      )}
+    />
+  );
+}
--- src/Telas/Home/index.js
+++ src/Telas/Home/index.js
@@ -1,20 +1,16 @@
 import React from "react";
-import { FlatList, StyleSheet, ScrollView } from "react-native";
-
-import Texto from "../../Components/Texto";
+import { ScrollView } from "react-native";
+import Carrossel from "./Components/Carrossel";
 
 export default function HomePage() {
+  const colorsCarrossel = [
+    "rgb(255,255,0)",
+    "rgb(65,65,65)",
+    "rgb(103, 20, 130)",
+  ];
   return (
     <ScrollView>
-      <Texto style={estilos.bemvindo}>Bem Vindo;</Texto>
-      <Texto style={estilos.bemvindo}></Texto>
+      <Carrossel data={colorsCarrossel} />
     </ScrollView>
   );
 }
-
-const estilos = StyleSheet.create({
-  bemvindo: {
-    fontSize: 20,
-    color: "black",
-  },
-});
