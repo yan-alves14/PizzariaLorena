61a4a2be69aaa73aca2dd234e125a2468153ef85
Leonardo Asciuti Carmagnani <lasciuti.c@gmail.com>
2023-06-16 12:38:26 -0300

    Botoes finalizados RET E DEL

--- src/Telas/Home/Components/BotaoRetirar.js
+++ src/Telas/Home/Components/BotaoRetirar.js
@@ -1,18 +1,69 @@
 import React from "react";
-import { StyleSheet, TouchableOpacity, Text } from 'react-native';
+import { StyleSheet, TouchableOpacity, Image, View } from 'react-native';
 
-export default function btnRetira(){
-    return <TouchableOpacity>
-        
-    </TouchableOpacity>
+import Texto from "../../../Components/Texto.js"
 
+export default function btnRetirar({ titulo, textoSecundario, icone, background, style }) {
+    return (
+      <TouchableOpacity style={estilos.button}>
+        <Image source={background} style={estilos.backgroundImage} resizeMode="cover" />
+        <View style={estilos.content}>
+          <Image source={icone} style={estilos.icon} />
+          <View style={estilos.textContainer}>
+            <Texto style={estilos.title}>{titulo}</Texto>
+            <Texto style={estilos.subtitle}>{textoSecundario}</Texto>
+          </View>
+        </View>
+      </TouchableOpacity>
+    );
+  }
     
-}
-
-const estilos = StyleSheet.create({
-    botao:{
-
-    }
 
 
-})
+  const estilos = StyleSheet.create({
+    button: {
+      width: 290,
+      height: 100,
+      borderRadius: 25,
+      overflow: 'hidden',
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#B82C21',
+      
+    },
+    backgroundImage: {
+      ...StyleSheet.absoluteFillObject,
+      height: '110%',
+    },
+    content: {
+      flexDirection: 'row',
+      alignItems: 'center',
+    },
+    icon: {
+      width: 70,
+      height: 70,
+      marginRight: 30,
+      marginLeft: 8
+    },
+    textContainer: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'flex-start',
+    },
+    title: {
+      fontSize: 16,
+      fontWeight: 'bold',
+      color: '#fff',
+      textTransform: 'uppercase',
+      textAlign: 'center',
+      marginBottom: 4,
+    },
+    subtitle: {
+      fontSize: 13,
+      color: '#fff',
+      textAlign: 'left',
+    },
+    texto: {
+      textAlign: 'left',
+    },
+  });
--- src/Telas/Home/index.js
+++ src/Telas/Home/index.js
@@ -2,6 +2,14 @@ import React from "react";
 import { ScrollView, Image, Dimensions } from "react-native";
 import Carrossel from "./Components/Carrossel";
 
+import BgDelivery from "../../../assets/fundo_btn.png"
+import BgRetirar from "../../../assets/fundo_btn.png"
+import IconMotoboy from "../../../assets/imgHome/icone_motoboy.png"
+import IconPizza from "../../../assets/imgHome/icone_btn_retirar.png"
+
+import BotaoDelivery from "../Home/Componentes/BotaoDelivery.js";
+import BotaoRetirar from "../Home/Componentes/BotaoRetirar.js"
+
 const { width } = Dimensions.get("window");
 
 export default function HomePage() {
@@ -19,6 +27,35 @@ export default function HomePage() {
         source={require("../../../assets/imgHome/Naturale-2.png")}
         style={{ width: width, height: width, marginTop: 30, marginBottom: 30 }}
       ></Image>
+      <View style={estilos.spaceTOP}/>
+        <View style={estilos.container}>
+          <BotaoDelivery
+          titulo="Prefiro delivery"
+          textoSecundario="Receba em casa, com rapidez e qualidade"
+          background={BgDelivery}
+          icone={IconMotoboy}/>
+        <View style={estilos.spaceBOT}/>
+          <BotaoRetirar
+          titulo="Prefiro retirar"
+          textoSecundario="Faça seu pedido e retire em nossa loja."
+          background={BgRetirar}
+          icone={IconPizza}/>
+    </View>
     </ScrollView>
   );
 }
+
+const estilos = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    },
+    spaceBOT:{
+      height: 16,
+    },
+    spaceTOP: {
+      height: 30,
+    }
+})
