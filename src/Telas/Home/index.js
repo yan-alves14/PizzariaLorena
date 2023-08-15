abba20f6979ea1ffafcd135762658ced5316082b
imyudz <victorhenrivargas11@gmail.com>
2023-08-14 21:54:33 -0300

    Mudando Textos e Adicionando novas fontes na tela inicial

--- src/Telas/Home/index.js
+++ src/Telas/Home/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { ScrollView, Image, Dimensions, StyleSheet, View } from "react-native";
+import { ScrollView, Image, Dimensions, StyleSheet, View, Text } from "react-native";
 import Carrossel from "./Components/Carrossel";
 
 import BgDelivery from "../../../assets/imgHome/fundo_btn.png";
@@ -44,12 +44,14 @@ export default function HomePage() {
     </View>
       <Image
         source={require("../../../assets/imgHome/Naturale-2.png")}
-        style={{ width: width, height: width, marginTop: 30, marginBottom: 30 }}
+        style={{ width: width, height: width, marginTop: 30}}
       ></Image>
-
+      <View style={estilos.Separator} />
+      <Text style={estilos.QuestionTitle}>Por que a Pizzaria Lorena?</Text>
       <Texto style={estilos.TextoDefault}>A Pizzaria Lorena é uma das melhores da região, destacando-se pelo seu atendimento, tradição, qualidade e profissionalidade de seus funcionários. Como nosso próprio slogan diz, a melhor da cidade!</Texto>
-      <View style={estilos.spaceTOP}/>
-        
+      <View style={estilos.Separator} />
+      <Text style={estilos.QuestionTitle}>Agora Mesmo!</Text>
+      <Texto style={estilos.Subtitle}>Agora que você sabe quem somos, aproveite e e peça nossa deliciosa pizza!</Texto>
     </ScrollView>
   );
 }
@@ -60,26 +62,61 @@ const estilos = StyleSheet.create({
     flexDirection: 'column',
     alignItems: 'center',
     justifyContent: 'center',
-    },
-    spaceBOT:{
-      height: 16,
-    },
-    spaceTOP: {
-      height: 30,
-    },
-    TextoDefault:{
-      margin: '5%',
-      textAlign: 'justify',
-      fontSize: 17,
-      color: '#808080'
-      //marginHorizontal:'10%',
-    },
-    TextoTitle:{
-      marginLeft: '2%',
-      marginRight: '2%',
-      marginHorizontal:'10%',
-      fontSize: 22,
-      paddingLeft: 35,
-      fontWeight:'bold',
-    }
+  },
+  spaceBOT:{
+    height: 16,
+  },
+  spaceTOP: {
+    height: 30,
+  },
+  TextoDefault:{
+    margin: '5%',
+    marginTop: 0,
+    marginBottom: 0,
+    paddingLeft: 12,
+    paddingRight: 12,
+    textAlign: 'center',
+    justifyContent: 'center',
+    fontSize: 17,
+    color: '#000',
+    fontWeight: '400',
+  },
+  TextoTitle:{
+    marginLeft: '2%',
+    marginRight: '2%',
+    marginHorizontal:'10%',
+    fontSize: 22,
+    paddingLeft: 35,
+    fontWeight:'bold',
+  },
+  Separator: {
+    width: 77,
+    flexShrink: 0,
+    height: 2.5,
+    backgroundColor: '#000',
+    borderRadius: 26,
+    alignSelf: "center",
+    marginTop: 15,
+    marginBottom: 15,
+  },
+  QuestionTitle: {
+    fontFamily: "GreatVibes",
+    color: "#000",
+    textAlign: "center",
+    textShadowColor: "#D77E19",
+    textShadowRadius: 6,
+    fontSize: 40,
+    letterSpacing: -0.54,
+    marginTop: 7,
+    marginBottom: 7,
+  },
+  Subtitle: {
+    color: "#000",
+    fontSize: 17,
+    fontWeight: 'bold',
+    letterSpacing: 0.22,
+    textAlign: "center",
+    padding: 20,
+    paddingTop: 0,
+  }
 })
